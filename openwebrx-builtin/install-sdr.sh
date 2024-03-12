0d47744f2f9a6dfff4ed1ff1cdb2adb7d7919bd8
CleversonSA <cleverson.assis@desegni.com.br>
2024-03-11 22:30:39 -0300

    1) Updated SDR Server scripts to create openwebrx admin user 2) Added few profiles for SDR 3) Added custom picture of the SDR

--- openwebrx-builtin/install-sdr.sh
+++ openwebrx-builtin/install-sdr.sh
@@ -7,11 +7,62 @@ if [ ! -e "/bin/docker" ]; then
 
 fi
 
+echo "Installing openwebrx docker container..."
 docker run --device /dev/bus/usb \
-        -p 8085:8073 \
+        -p 8086:8073 \
         -v "${PWD}/:/var/lib/openwebrx/" \
         -d \
         --restart no \
         --name uverkisto-webrx \
         --memory=256M \
         jketterl/openwebrx:stable
+docker start uverkisto-webrx
+
+
+echo "Checking container status..."
+DOCKER_CONTAINER_RUNNING=`docker ps | grep uverkisto-webrx`
+if [ "$DOCKER_CONTAINER_RUNNING" == "" ]; then
+  
+   /bin/dialog --title 'Install error' \
+	   --msgbox 'It was not possible to install and run openwebrx container, please check docker settings!' \
+   	  0 0
+   clear 
+   exit -1
+   
+fi
+
+echo "Adding administrator user..."
+ADMIN_USER_STATUS=`docker exec -t uverkisto-webrx ./openwebrx.py admin listusers | grep uverkisto`
+if [ "$ADMIN_USER_STATUS" == "" ]; then
+
+   echo "Creating uverkisto user..."
+   /bin/sleep 1
+   docker exec -i -t uverkisto-webrx ./openwebrx.py admin adduser uverkisto
+
+else
+
+   echo "Resetting uverkisto password..."
+   /bin/sleep 1
+   docker exec -i -t uverkisto-webrx ./openwebrx.py admin resetpassword uverkisto
+
+fi
+
+echo "Checking uverkisto user..."
+ADMIN_USER_CREATED=`docker exec -t uverkisto-webrx ./openwebrx.py admin listusers | grep uverkisto`
+if [ "$ADMIN_USER_CREATED" == "" ]; then
+
+   /bin/dialog --title 'Install error' \
+           --msgbox 'It was not possible to create the\nadmin user, please try again!!' \
+          0 0
+   clear 
+   exit -1
+
+fi
+
+
+
+echo "Done"
+
+/bin/sleep 2
+exit 0
+
--- openwebrx-builtin/start-sdr.sh
+++ openwebrx-builtin/start-sdr.sh
@@ -9,8 +9,22 @@ fi
 
 docker start uverkisto-webrx
 
-docker ps 
+echo "Checking container status..."
+DOCKER_CONTAINER_RUNNING=`docker ps | grep uverkisto-webrx`
+if [ "$DOCKER_CONTAINER_RUNNING" == "" ]; then
 
+   /bin/dialog --title 'Install error' \
+           --msgbox 'It was not possible to run openwebrx container,\n please check docker settings!' \
+          0 0
+   clear
+   exit -1
+
+fi
+
+/bin/dialog --title 'SDR Server' \
+	--infobox 'Server running...' \
+	0 0
 /bin/sleep 2
 
+clear
 exit 0
